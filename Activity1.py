84039efe9b20b1515c19e4a7157da57575afb0fb
29sroy-ship-it <29sroy@mkis.edu.my>
2025-09-28 14:27:05 +0800

    Print statments

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+#printing a sentence
+print ("Hello, welcome to the world of programming")
+#printing a number
+print (9)
+#using \n and new line
+print ("Im 14 years \n old")
+#passing multiple arguements for printing
+print ("Hello, Im", 14)
+#end statement
+print ("I like football", end="*")
+print ("and reading books")
